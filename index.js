b6e05d6b64885ad99d58617cec9ac2cc9d5eedf1
Kirill Ivashkov <kirillivashk@gmail.com>
2024-05-15 17:05:05 +0300

    uibuild

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -62,27 +64,30 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing',
-    });
-  }
+  // if (!body.name) {
+  //   return response.status(400).json({
+  //     error: 'name missing',
+  //   });
+  // }
 
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing',
-    });
-  }
+  // if (!body.number) {
+  //   return response.status(400).json({
+  //     error: 'number missing',
+  //   });
+  // }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => response.status(201).json(savedPerson));
+  person
+    .save()
+    .then((savedPerson) => response.status(201).json(savedPerson))
+    .catch((error) => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -93,7 +98,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
